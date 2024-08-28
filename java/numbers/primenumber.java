6f0b88237a7c15508dc9e3c36612854f41af8557
GREENGANGA <GreeN11.dev@gmail.com>
2024-08-28 13:49:53 +0530

    2

--- java/numbers/primenumber.java
+++ java/numbers/primenumber.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class primenumber {
+public class primenumber {
+    public static void main(String[] args){
+        int n =7;
+        int checkprime =0;
+
+        for(int i=2;i<n;i++){
+            int x = n%i;
+            if(x==0){
+                checkprime++;
+                break;
+            }
+        }
+        if(checkprime==0){
+            System.out.println("The given number is a prime number");
+        }else {
+            System.out.println("The given number is not a prime number");
+        }
+    }
 }
+//Outout --> The given number is a prime number
--- java/numbers/reversenumber.java
+++ java/numbers/reversenumber.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class reversenumber {
+//reverse a number
+public class reversenumber {
+    public static void main(String[] args){
+        int n = 12345;
+        int rev = 0;
+
+        while(n!=0){
+            int x = n%10;
+            System.out.print(x);
+            n = n/10;
+        }
+    }
 }
+
+//outbut --> 54321
